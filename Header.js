35d344090af05181c1b0c1f5502c514a3a6de27b
w0lfschild <aguywithlonghair@yahoo.com>
2018-08-25 14:02:22 -0700

    Bump
    - New header
    - Updated changelog

--- Header.js
+++ Header.js
@@ -23,69 +23,76 @@ document.write('\
     <![endif]-->         \
     </head>     \
     <body data-spy="scroll" data-target="nav">\
-        <header id="header-2" class="soft-scroll header-2">\
-</header>\
-        <nav class="navbar navbar-default navbar-fixed-top" role="navigation"> \
-            <div class="container-fluid"> \
-                <div class="navbar-header">\
-                    <button class="navbar-toggle" data-toggle="collapse" data-target="#nav1">\
-                        <span class="sr-only">Toggle navigation</span> \
-                        <span class="icon-bar"></span> \
-                        <span class="icon-bar"></span> \
-                        <span class="icon-bar"></span>\
-                    </button>                     \
-                    <a href="#" class="navbar-brand finn"><i class="fas fa-terminal"></i> macEnhance</a> \
-                </div>                 \
-                <div class="collapse navbar-collapse" id="nav1">\
-                    <ul class="nav navbar-nav navbar-right"> \
-                        <li>\
-                            <a href="https://www.macenhance.com"><i class="fas fa-home"></i> Home</a>\
-                        </li>                         \
-                        <li class="dropdown"> \
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-html="false"><i class="fab fa-app-store-ios"></i> Applications <i class="fas fa-angle-down"></i></a> \
-                            <ul class="dropdown-menu"> \
-                                <li>\
-                                    <a href="https://cdock.macenhance.com"><i class="fas fa-paint-brush"></i> cDock</a>\
-                                </li>\
-                                <li>\
-                                    <a href="https://www.macenhance.com/macforge"><i class="fas fa-cubes"></i> MacForge</a>\
-                                </li>\
-                                <li>\
-                                    <a href="https://www.macenhance.com/flashlight"><i class="fas fa-lightbulb"></i> Flashlight</a>\
-                                </li>\
-                                <li>\
-                                    <a href="https://www.macenhance.com/darkboot"><i class="fas fa-moon"></i> DarkBoot</a>\
-                                </li>\
-                                <li>\
-                                    <a href="https://www.macenhance.com/classicfolders"><i class="far fa-folder"></i> Classic Folders</a>\
-                                </li>\
-                                <li>\
-                                    <a href="https://www.macenhance.com/vpkmacos"><i class="fas fa-exchange-alt"></i> VPK Converter</a>\
-                                </li>                                 \
-                            </ul>                             \
-                        </li>\
-                        <li class="dropdown"> \
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fas fa-cube"></i> Plugins <i class="fas fa-angle-down"></i></a> \
-                            <ul class="dropdown-menu"> \
-                                <li>\
-                                    <a href="#">Coming soon...</a>\
-                                </li>                                 \
-                            </ul>                             \
-                        </li>                         \
-                        <li>\
-                            <a href="mailto:aguywithlonghair@gmail.com?Subject=w0lfHub"><i class="fas fa-envelope"></i> Contact</a>\
-                        </li>\
-                        <li>\
-                            <a href="http://goo.gl/DSyEFR" class="bg-orange white"><i class="fab fa-paypal"></i> Donate</a>\
-                        </li>\
-                    </ul>\
+        <header id="header-1" class="soft-scroll header-1">\
+            <!-- Navbar -->\
+            <nav class="main-nav navbar-fixed-top headroom headroom--pinned">\
+                <div class="container">\
+                    <!-- Brand and toggle -->\
+                    <div class="navbar-header">\
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">\
+                            <span class="sr-only">Toggle navigation</span>\
+                            <span class="icon-bar"></span>\
+                            <span class="icon-bar"></span>\
+                            <span class="icon-bar"></span>\
+                        </button>\
+                        <a href="#" class="navbar-brand offwhite orange-hover margin-top10"><i class="fas fa-terminal"></i> macEnhance</a> \
+                    </div>\
+                    <!-- Navigation -->\
+                    <div class="collapse navbar-collapse">\
+                        <ul class="nav navbar-nav navbar-right">\
+                            <li class="nav-item">\
+                                <a href="https://www.macenhance.com" class="orange-hover"><i class="fas fa-home"></i> Home</a>\
+                            </li>\
+                            <li class="nav-item dropdown">\
+                                <a class="dropdown-toggle orange-hover" data-toggle="dropdown" data-hover="dropdown" data-delay="0" data-close-others="false" href="#"><i class="fab fa-app-store-ios"></i> Applications <i class="fas fa-angle-down"></i></a>\
+                                <ul class="dropdown-menu">\
+                                    <li>\
+                                        <a href="https://cdock.macenhance.com"><i class="fas fa-paint-brush"></i> cDock</a>\
+                                    </li>\
+                                    <li>\
+                                        <a href="https://www.macenhance.com/macforge"><i class="fas fa-cubes"></i> MacForge</a>\
+                                    </li>\
+                                    <li>\
+                                        <a href="https://www.macenhance.com/flashlight"><i class="fas fa-lightbulb"></i> Flashlight</a>\
+                                    </li>\
+                                    <li>\
+                                        <a href="https://www.macenhance.com/darkboot"><i class="fas fa-moon"></i> DarkBoot</a>\
+                                    </li>\
+                                    <li>\
+                                        <a href="https://www.macenhance.com/classicfolders"><i class="far fa-folder"></i> Classic Folders</a>\
+                                    </li>\
+                                    <li>\
+                                        <a href="https://www.macenhance.com/vpkmacos"><i class="fas fa-exchange-alt"></i> VPK Converter</a>\
+                                    </li>                                     \
+                                </ul>                                 \
+                            </li>\
+                            <!--//dropdown-->                             \
+                            <li class="nav-item dropdown">\
+                                <a class="dropdown-toggle orange-hover" data-toggle="dropdown" data-hover="dropdown" data-delay="0" data-close-others="false" href="#"><i class="fas fa-cube"></i> Plugins <i class="fas fa-angle-down"></i></a>\
+                                <ul class="dropdown-menu">\
+                                    <li>\
+                                        <a href="#">Coming soon...</a>\
+                                    </li>                                     \
+                                </ul>                                 \
+                            </li>\
+                            <!--//dropdown-->                             \
+                            <li class="nav-item">\
+                                <a href="mailto:aguywithlonghair@gmail.com?Subject=w0lfHub" class="orange-hover"><i class="fas fa-envelope"></i> Contact</a>\
+                            </li>\
+                            <li class="nav-item">\
+                                <a href="http://goo.gl/DSyEFR" class="bg-orange white marina-hover"><i class="fab fa-paypal"></i> Donate</a>\
+                            </li>\
+                        </ul>\
+                        <!--//nav-->\
+                    </div>\
+                    <!--// End Navigation -->\
                 </div>\
-            </div>             \
-        </nav>\
+                <!--// End Container -->\
+            </nav>\
+            <!--// End Navbar -->\
+        </header>\
         <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>         \
         <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>         \
         <script type="text/javascript" src="js/plugins.js"></script>\
-        <script type="text/javascript" src="js/bskit-scripts.js"></script>         \
-    </body>     \
-</html>\
+        <script type="text/javascript" src="js/bskit-scripts.js"></script>\
 ');
