8f8bd82e6e1934c50c66f1e00ce7188f838e54c1
Wolfgang Baird <aguywithlonghair@yahoo.com>
2020-08-10 18:19:58 -0700

    Update
    - lowercase

---
+++ header.js
@@ -0,0 +1,70 @@
+document.write('\
+<!DOCTYPE html>\
+<html>\
+\
+<head>\
+    <meta charset="utf-8">\
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">\
+    <title>macEnhance</title>\
+    <meta name="twitter:card" content="summary_large_image">\
+    <meta property="og:description" content="The tools you need to enhance your macOS experience">\
+    <meta name="description" content="The tools you need to enhance your macOS experience">\
+    <meta property="og:type" content="website">\
+    <meta property="og:title" content="macEnhance">\
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">\
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:300,400,700">\
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">\
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">\
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">\
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">\
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">\
+    <link rel="stylesheet" href="assets/css/styles.min.css">\
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">\
+</head>\
+\
+<body>\
+    <!-- Start: Header - Apple -->\
+    <nav class="navbar navbar-dark navbar-expand-md fixed-top bg-dark">\
+        <div class="container"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>\
+            <div class="collapse navbar-collapse" id="navcol-1"><a class="navbar-brand" style="font-size:24px;" href="https://www.macenhance.com"><i class="fa fa-terminal"></i>&nbsp;MacEnhance</a>\
+                <ul class="nav navbar-nav flex-grow-1 justify-content-end" style="font-family: Roboto, sans-serif;">\
+                    <li class="nav-item" role="presentation">\
+                        <!-- Start: HoverDownButton --><div class="dropdown">\
+  <button class="dropbtn"><i class="fab fa-app-store-ios"></i> Apps <i class="fas fa-angle-down"></i></button>\
+  <div class="dropdown-content">\
+    <a href="https://cdock.macenhance.com"><i class="fas fa-palette"></i> cDock</a>\
+    <a href="https://www.macenhance.com/dockmate"><i class="far fa-window-restore"></i> Dock Mate</a>\
+    <a href="https://www.macenhance.com/macforge"><i class="fas fa-cube"></i> MacForge</a>\
+    <a href="https://www.todeska.app"><i class="fas fa-desktop"></i> Todeska</a>\
+<!--     <a href="https://www.macenhance.com/bootbuddy"><i class="fas fa-adjust"></i> Boot Buddy</a> -->\
+  </div>\
+</div>\
+                        <!-- End: HoverDownButton -->\
+                    </li>\
+                    <li class="nav-item" role="presentation">\
+                        <!-- Start: HoverDownButton --><div class="dropdown">\
+  <button class="dropbtn"><i class="fas fa-hat-cowboy-side"></i> Legacy <i class="fas fa-angle-down"></i></button>\
+  <div class="dropdown-content">\
+    <a href="https://www.macenhance.com/flashlight"><i class="far fa-lightbulb"></i> &nbsp; Flashlight</a>\
+    <a href="https://www.macenhance.com/classicfolders"><i class="far fa-folder"></i> &nbsp;Classic Folders</a>\
+    <a href="https://www.macenhance.com/vpkmacos"><i class="fas fa-exchange-alt"></i> &nbsp;VPK Converter</a>\
+  </div>\
+</div>\
+                        <!-- End: HoverDownButton -->\
+                    </li>\
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="https://www.macenhance.com/contact"><i class="fa fa-send"></i>&nbsp;Contact</a></li>\
+                    <li class="nav-item" role="presentation"><a class="nav-link cust" href="https://www.macenhance.com/about"><i class="fas fa-info-circle"></i>&nbsp;About</a></li>\
+                </ul>\
+            </div>\
+        </div>\
+    </nav>\
+    <!-- End: Header - Apple -->\
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>\
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>\
+    <script src="assets/js/smart-forms.min.js"></script>\
+    <script src="assets/js/script.min.js"></script>\
+</body>\
+\
+</html>\
+');
